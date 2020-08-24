24d601f5000e6d3d6c38e1be9e401529afcdca75
Greg <remph819@gmail.com>
2020-08-23 21:08:09 -0700

    finished slices

---
+++ slices/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := 43
+	xi := []int{x, y} //slice of int
+
+	a := "The morning sun"
+	b := "The evening rocks"
+	c := "Good morning Vietnam"
+	xp := []string{a, b, c} //slice of string
+
+	//Cannot combine the two
+	fmt.Println(xi)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%T\n", xi)
+
+	fmt.Println(xp)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+	fmt.Printf("%T\n", xp)
+}
