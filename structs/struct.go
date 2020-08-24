7d8c32338a89da219687b775a9043accd588d48f
Greg <remph819@gmail.com>
2020-08-24 10:16:45 -0700

    updated structs

--- structs/struct.go
+++ structs/struct.go
@@ -19,6 +19,20 @@ func main() {
 		license: 007,
 		sayings: []string{"Shaken, not stirred", "Bond, James Bond"},
 	}
+
+	p2 := person{
+		first:   "Jenny",
+		license: 8,
+		sayings: []string{"When Bond can't, I can", "I laugh in the face of danger"},
+	}
+	fmt.Println(p2)
+
+	xp := []person{p1, p2}
+	fmt.Println(xp)
+
+	mp := map[string]person{"Mr": p1, "Ms": p2}
+	fmt.Println(mp)
+
 	x = 7
 	fmt.Printf("%T\n", x)
 	fmt.Println(x)
